37c1eab59dfcf6bb792e2df89c06026d95ad7e2f
DyProject <br32108@gmail.com>
2022-06-17 15:50:10 -0300

    Seção 2 - Criando servidor HTTP simples

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var Calculadora = require("./calculadora");
+var http = require("http");
 
 console.log(Calculadora.mult(10, 5));
 console.log(Calculadora.nome);
+
+http.createServer(function(requisicao, resposta){
+    resposta.end("<h1>Bem vindo ao meu site!</><h4>guiadoprogramador.com</h4>")
+}).listen(3000)
+
+console.log("Meu servidor está rodando!")
