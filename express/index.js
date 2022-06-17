71f686250e45dfb6e74cb41f39d3fd30dd92071c
DyProject <br32108@gmail.com>
2022-06-17 16:18:08 -0300

    Seção 3 - Rotas

--- express/index.js
+++ express/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(req,res){
+    res.send("Bem vindo ao guia do programador")
+})
+
 app.listen(4000, function(erro){
     if(erro){
-        console.log("Ocorreu um erro")
+        console.log("Ocorreu um erro");
     }else{
-        console.log("Servidor Rodando")
+        console.log("Servidor Rodando");
     }
 })
